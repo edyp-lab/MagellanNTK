2d5baa67e438f6fb7e5dc0204ca7af52211c1f5d
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2025-07-23 11:20:47 +0200

    Update for Do Proceed buttons

--- R/process_layout.R
+++ R/process_layout.R
@@ -2,34 +2,34 @@
 process_layout <- function(sidebar, content){
   
   div(
-  style = "position: relative;",
-  
-  absolutePanel(
-    top = MagellanNTK::default.layout$top_panel,
-    left = MagellanNTK::default.layout$left_panel,
-    width = MagellanNTK::default.layout$width_panel,
-    height = "100%",
-    fixed = TRUE,
-    #style = "z-index: 0;",
-    div(
-      style = "",
-      tagList(
-        content
-      )
-    )
-  ),
+  style = "position: relative; z-index: 0;",
     absolutePanel(
-      top = MagellanNTK::default.layout$top_sidebar,
-      left = MagellanNTK::default.layout$left_sidebar,
-      width = MagellanNTK::default.layout$width_sidebar,
+      top = MagellanNTK::default.layout$top_panel,
+      left = MagellanNTK::default.layout$left_panel,
+      width = MagellanNTK::default.layout$width_panel,
       height = "100%",
       fixed = TRUE,
-      style = paste0("background:", default.layout$bgcolor_sidebar, ";"),
+      #style = "padding:15px; background:lightblue;",
       div(
-        style = "margin-top:55px;",
-        sidebar
+        style = "z-index: 0;",
+        tagList(
+          content
+        )
       )
+    ),
+    absolutePanel(
+    top = MagellanNTK::default.layout$top_sidebar,
+    left = MagellanNTK::default.layout$left_sidebar,
+    width = MagellanNTK::default.layout$width_sidebar,
+    height = "100%",
+    fixed = TRUE,
+    style = paste0("background:", default.layout$bgcolor_sidebar, ";"),
+    div(
+      style = "margin-top:55px;",
+      sidebar
     )
+  )
+  
   
 )
 
