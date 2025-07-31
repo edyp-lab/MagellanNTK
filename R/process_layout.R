e72312227815bdabf12db9471638b7e6deecedc0
Samuel Wieczorek <samuel.wieczorek@cea.fr>
2025-07-31 16:20:02 +0200

    Update for layout in MagellanNTK

--- R/process_layout.R
+++ R/process_layout.R
@@ -1,6 +1,9 @@
 #' @export
 process_layout <- function(ns, sidebar, content){
   
+  window_height <- htmlwidgets::JS('window.innerHeight')
+  window_width <- Jhtmlwidgets::S('window.innerWidth')
+  
   div(
     div(
       sidebar
@@ -10,6 +13,7 @@ process_layout <- function(ns, sidebar, content){
       style = "position: absolute; }",
       top = 76,
       left = 255,
+      width = window_width - 255,
       div(
         content
       )
